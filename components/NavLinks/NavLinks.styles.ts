d54987032df3542ef9f7210d5f3349e2174deb67
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-10-23 21:37:10 -0300

    Delete components directory

--- components/NavLinks/NavLinks.styles.ts
+++
@@ -1,8 +0,0 @@
-import { Nav } from "react-bootstrap";
-import styled from 'styled-components';
-
-
-export const StyledNavLink = styled(Nav.Link)`
-  color: ${props => props.theme.colors.text};
-  text-decoration: none;
-`;
