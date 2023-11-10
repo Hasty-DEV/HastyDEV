46cb07cd595d1b3f0f843e28191578e8348a7ad4
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2023-11-10 19:00:10 -0300

    Retirando  import não usado

--- frontend/src/routes/Login/Login.tsx
+++ frontend/src/routes/Login/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Formik, Field } from "formik";
 import * as yup from "yup";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   FacebookLoginButton,
   GoogleLoginButton,
