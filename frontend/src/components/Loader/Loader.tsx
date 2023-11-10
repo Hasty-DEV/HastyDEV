972775b21ea21d6821b87000bd3de16886b30a18
Jefflds <Jeffersonlds3009@gmail.com>
2023-11-10 11:35:13 -0300

    Adicionando Loader no Register

--- frontend/src/components/Loader/Loader.tsx
+++ frontend/src/components/Loader/Loader.tsx
@@ -1,34 +1,65 @@
 import { styled } from "styled-components";
 
 const Loader = () => {
-  return <LoaderStyled></LoaderStyled>;
+  return (
+    <LoaderOverlay>
+      <LoaderStyled>
+        <div className="loader-inner"></div>
+      </LoaderStyled>
+    </LoaderOverlay>
+  );
 };
 
 export default Loader;
 
-const LoaderStyled = styled.span`
-  width: 48px;
-  height: 48px;
+const LoaderOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5); 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999; 
+  backdrop-filter: blur(5px); 
+`;
+
+const LoaderStyled = styled.div`
+  width: 64px;
+  height: 64px;
   border-radius: 50%;
-  display: inline-block;
-  border-top: 4px solid #fff;
-  border-right: 4px solid transparent;
-  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: rgba(0, 0, 0, 0.7); 
   animation: rotation 1s linear infinite;
 
-  &:after {
-    content: "";
-    box-sizing: border-box;
-    position: absolute;
-    left: 0;
-    top: 0;
+  .loader-inner {
     width: 48px;
     height: 48px;
     border-radius: 50%;
-    border-left: 4px solid #ff3d00;
-    border-bottom: 4px solid transparent;
-    animation: rotation 0.5s linear infinite reverse;
+    border-top: 4px solid #fff;
+    border-right: 4px solid transparent;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite reverse;
+
+    &:after {
+      content: "";
+      box-sizing: border-box;
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 48px;
+      height: 48px;
+      border-radius: 50%;
+      border-left: 4px solid #ff3d00;
+      border-bottom: 4px solid transparent;
+      animation: rotation 0.5s linear infinite;
+    }
   }
+
   @keyframes rotation {
     0% {
       transform: rotate(0deg);
--- frontend/src/routes/Register/Register.tsx
+++ frontend/src/routes/Register/Register.tsx
@@ -5,10 +5,11 @@ import RegisterImg from "../../assets/images/RegisterImg.png";
 import { FormFetch } from "../../axios/config";
 import { Formik, Field } from "formik";
 import { Link, useNavigate } from "react-router-dom";
-import swal  from 'sweetalert2';
+import swal from "sweetalert2";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import Loader from "../../components/Loader/Loader";
+import { useState } from "react";
 
 interface FormValues {
   username: string;
@@ -21,126 +22,158 @@ interface FormValues {
 const validationsRegister = yup.object().shape({
   first_name: yup.string().required("O campo de nome é obrigatório"),
   last_name: yup.string().required("O campo de sobrenome é obrigatório"),
-  username: yup.string().required("O campo de nome de usuário é obrigatório").min(5, "O username deve ter pelo menos 5 caracteres"),
+  username: yup
+    .string()
+    .required("O campo de nome de usuário é obrigatório")
+    .min(5, "O username deve ter pelo menos 5 caracteres"),
   email: yup.string().email("Email inválido").required("O email é obrigatório"),
-  password: yup.string()
+  password: yup
+    .string()
     .required("A senha é obrigatória")
     .min(6, "A senha deve ter pelo menos 6 caracteres")
     .matches(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
     .matches(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
     .matches(/[0-9]/, "A senha deve conter pelo menos um número")
-    .matches(/[@$!¨%*#?&]/, "A senha deve conter pelo menos um caractere especial"),
-    confirmPassword: yup.string().required("A confirmação da senha é obrigatória").oneOf([yup.ref("password")], "As senhas não coincidem"),
+    .matches(
+      /[@$!¨%*#?&]/,
+      "A senha deve conter pelo menos um caractere especial"
+    ),
+  confirmPassword: yup
+    .string()
+    .required("A confirmação da senha é obrigatória")
+    .oneOf([yup.ref("password")], "As senhas não coincidem"),
 });
 
 const Register: React.FC = () => {
+  const [registrationInProgress, setRegistrationInProgress] = useState(false);
+  const [registrationSuccess, setRegistrationSuccess] = useState(false);
 
-  
-  const navigate = useNavigate(); 
-  const handleRegister = async ({ first_name, last_name, username, email, password, confirmPassword }: FormValues) => {
+  const navigate = useNavigate();
+  const handleRegister = async ({
+    first_name,
+    last_name,
+    username,
+    email,
+    password,
+    confirmPassword,
+  }: FormValues) => {
     try {
-      const response = await FormFetch.post("/register", { first_name, last_name, username, email, password, confirmPassword })
+      setRegistrationInProgress(true);
+      const response = await FormFetch.post("/register", {
+        first_name,
+        last_name,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
       console.log(response.data);
- 
-        swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Seu cadastro foi realizado com sucesso',
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      
-    
-      navigate('/login');
- 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Seu cadastro foi realizado com sucesso",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setRegistrationSuccess(true);
+      navigate("/login");
     } catch (err: any) {
+      setRegistrationInProgress(false);
       toast.error(err.response.data.error);
       alert(err.response.data.errors[0].msg);
       alert(err.response.data.error);
     }
-
   };
 
   return (
     <Container fluid>
       <Row>
         <Col>
-          <img
-            src={RegisterImg}
-            alt=""
-            height="auto"
-            width="90%"
-          />
+          <img src={RegisterImg} alt="" height="auto" width="90%" />
         </Col>
         <Col>
           <R.RegisterForm>
-            <Formik
-              initialValues={{
-                username: "",
-                password: "",
-                first_name: "",
-                last_name: "",
-                email: "",
-                confirmPassword: "",
-              }}
-              onSubmit={handleRegister}
-              validationSchema={validationsRegister}
-            >
-              {({ errors, touched, handleSubmit, isSubmitting }) => (
-                <>
-                  <form onSubmit={handleSubmit}>
-                    <h2>Inscreva-se na HastyDEV!</h2>
-                    <div className="form-group">
-                      <label htmlFor="first_name">Nome:</label>
-                      <Field type="text" id="first_name" name="first_name" />
-                      {errors.first_name && touched.first_name && errors.first_name}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="last_name">Sobrenome:</label>
-                      <Field type="text" id="last_name" name="last_name" />
-                      {errors.last_name && touched.last_name && errors.last_name}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="username">Usuário:</label>
-                      <Field type="text" id="username" name="username" />
-                      {errors.username && touched.username && errors.username}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="email">E-mail:</label>
-                      <Field type="email" id="email" name="email" />
-                      {errors.email && touched.email && errors.email}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="password">Senha:</label>
-                      <Field type="password" id="password" name="password" />
-                      {errors.password && touched.password && errors.password}
-                    </div>
-                    <div className="form-group">
-                      <label htmlFor="confirmPassword">
-                        Confirme Sua Senha:
-                      </label>
-                      <Field
-                        type="password"
-                        id="confirmPassword"
-                        name="confirmPassword"
-                      />
-                      {errors.confirmPassword && touched.confirmPassword && errors.confirmPassword}
-                    </div>
-                    
-                    <div className="form-group">
-                      <button type="submit" disabled={isSubmitting}>Inscreva-se</button>
-                    </div>
-                  </form>
-                  <div className="sign_in">
-                    <Link to="/login"  >
-                      <a>Já Tem uma Conta? Login</a>
-                    </Link>
-                  </div>
+            {registrationInProgress ? (
+              <Loader />
+            ) : registrationSuccess ? (
+              <>
+                <h2>Cadastro realizado com sucesso!</h2>
+                <div className="sign_in">
+                  <Link to="/login">
+                    <span>Já Tem uma Conta? Login</span>
+                  </Link>
+                </div>
+              </>
+            ) : (
+              <Formik
+                initialValues={{
+                  username: "",
+                  password: "",
+                  first_name: "",
+                  last_name: "",
+                  email: "",
+                  confirmPassword: "",
+                }}
+                onSubmit={handleRegister}
+                validationSchema={validationsRegister}
+              >
+                {({ errors, touched, handleSubmit, isSubmitting }) => (
+                  <>
+                    <form onSubmit={handleSubmit}>
+                      <h2>Inscreva-se na HastyDEV!</h2>
+                      <div className="form-group">
+                        <label htmlFor="first_name">Nome:</label>
+                        <Field type="text" id="first_name" name="first_name" />
+                        {errors.first_name &&
+                          touched.first_name &&
+                          errors.first_name}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="last_name">Sobrenome:</label>
+                        <Field type="text" id="last_name" name="last_name" />
+                        {errors.last_name &&
+                          touched.last_name &&
+                          errors.last_name}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="username">Usuário:</label>
+                        <Field type="text" id="username" name="username" />
+                        {errors.username && touched.username && errors.username}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="email">E-mail:</label>
+                        <Field type="email" id="email" name="email" />
+                        {errors.email && touched.email && errors.email}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="password">Senha:</label>
+                        <Field type="password" id="password" name="password" />
+                        {errors.password && touched.password && errors.password}
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="confirmPassword">
+                          Confirme Sua Senha:
+                        </label>
+                        <Field
+                          type="password"
+                          id="confirmPassword"
+                          name="confirmPassword"
+                        />
+                        {errors.confirmPassword &&
+                          touched.confirmPassword &&
+                          errors.confirmPassword}
+                      </div>
 
-                </>
-              )}
-            </Formik>
+                      <div className="form-group">
+                        <button type="submit" disabled={isSubmitting}>
+                          Inscreva-se
+                        </button>
+                      </div>
+                    </form>
+                  </>
+                )}
+              </Formik>
+            )}
           </R.RegisterForm>
         </Col>
       </Row>
