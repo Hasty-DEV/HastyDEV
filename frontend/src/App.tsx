3cfde1fb9cbbcb6ff631ab6e4e61de33cbb69e06
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-10 20:15:57 -0300

    Adicionando metricas da vercel no codigo.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Pages from "./Pages/index";
 import { AuthProvider } from "./Data/Contexts/Auth/AuthProvider";
 import usePersisteState from "./Data/Hooks/PersisteState/usePersisteState";
 import light from "./Ui/styles/themes/light";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   const [theme, setTheme] = usePersisteState("themes", light);
@@ -14,6 +16,8 @@ function App() {
         <Pages theme={theme} setTheme={setTheme} />
       </AuthProvider>
       <GlobalStyle />
+      <Analytics />
+      <SpeedInsights />
     </ThemeProvider>
   );
 }
