c119065da65f55790a43730bfcde9256f0317562
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-10 21:29:25 -0300

    Arrumando semantica

--- frontend/src/Ui/Partials/Header/Header.styles.ts
+++ frontend/src/Ui/Partials/Header/Header.styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.header`
   background: ${(props) => props.theme.colors.primary};
   position: fixed;
   width: 100%;
