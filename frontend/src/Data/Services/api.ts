6113dd3b7293ece45dffefc041c08305f7d21b25
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-16 22:40:42 +0000

    Arrumando config da api para permitir usar codigo no codespace

--- frontend/src/Data/Services/api.ts
+++ frontend/src/Data/Services/api.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const nodeEnv = import.meta.env.VITE_NODE_ENV;
-let baseURL;
-
-if (nodeEnv === "development") {
-  baseURL = "http://localhost:3001/api";
-} else if (nodeEnv === "production") {
-  baseURL = import.meta.env.VITE_API_URL;
-}
+let baseURL = import.meta.env.VITE_API_URL;
 
 // Criando a instância do axios com a URL base configurada
 export const api = axios.create({ baseURL });
--- frontend/src/Data/Services/axios/config.ts
+++ frontend/src/Data/Services/axios/config.ts
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const nodeEnv = import.meta.env.VITE_NODE_ENV;
-let URL;
-
-if (nodeEnv === "development") {
-  URL = "http://localhost:3001/api";
-} else if (nodeEnv === "production") {
-  URL = import.meta.env.VITE_API_URL;
-}
+let URL = import.meta.env.VITE_API_URL;
 
 export const FormFetch = axios.create({
   baseURL: `${URL}`,
