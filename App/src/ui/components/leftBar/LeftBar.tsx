1659bb99f725cef37a7a925dee715cfe09b65646
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-16 00:55:31 -0300

    Retirando items desncessarios

--- App/src/ui/components/leftBar/LeftBar.tsx
+++ App/src/ui/components/leftBar/LeftBar.tsx
@@ -1,16 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Friends from "../../assets/1.png";
-import Groups from "../../assets/2.png";
-import Market from "../../assets/3.png";
-import Watch from "../../assets/4.png";
-import Memories from "../../assets/5.png";
 import Events from "../../assets/6.png";
 import Gaming from "../../assets/7.png";
 import Gallery from "../../assets/8.png";
 import Videos from "../../assets/9.png";
 import Messages from "../../assets/10.png";
-import Tutorials from "../../assets/11.png";
-import Fund from "../../assets/13.png";
 import UserIcon from "../../assets/user/user_icon.png";
 import LeftBarContainer from "../../styles/leftBar/LeftBar.styles";
 import { useCallback, useEffect, useState } from "react";
@@ -67,26 +60,6 @@ const LeftBar = () => {
                     : "Usuário"}
                 </span>
               </div>
-              <div className="item">
-                <img src={Friends} alt="" />
-                <span>Home</span>
-              </div>
-              <div className="item">
-                <img src={Groups} alt="" />
-                <span>Em Construção...</span>
-              </div>
-              <div className="item">
-                <img src={Market} alt="" />
-                <span>Em Construção...</span>
-              </div>
-              <div className="item">
-                <img src={Watch} alt="" />
-                <span>Em Construção...</span>
-              </div>
-              <div className="item">
-                <img src={Memories} alt="" />
-                <span>Em Construção...</span>
-              </div>
             </div>
             <hr />
             <div className="menu">
@@ -113,17 +86,6 @@ const LeftBar = () => {
               </div>
             </div>
             <hr />
-            <div className="menu">
-              <span>Em Construção...</span>
-              <div className="item">
-                <img src={Fund} alt="" />
-                <span>Em Construção...</span>
-              </div>
-              <div className="item">
-                <img src={Tutorials} alt="" />
-                <span>Em Construção...</span>
-              </div>
-            </div>
             <div className="menu mt-3">
               <Button variant="danger" onClick={handleLogout}>
                 Logout
