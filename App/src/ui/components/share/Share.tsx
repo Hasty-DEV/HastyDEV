29102252ee88a17962bd909b8fb28c2f42cb6ceb
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-02-22 01:28:03 +0000

    adicionando loading do post

--- App/src/ui/components/share/Share.tsx
+++ App/src/ui/components/share/Share.tsx
@@ -1,12 +1,14 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import Image from "../../assets/img.png";
 import UserIcon from "../../assets/user/user_icon.png";
 import ShareContainer from "../../styles/share/Share.styles";
 import { api } from "../../../data/services/api";
+import Loader from "../Loader/Loader";
 
 const Share = () => {
   const titleRef = useRef<HTMLInputElement>(null);
   const contentRef = useRef<HTMLInputElement>(null);
+  const [loading, setLoading] = useState(false);
 
   const handleOnSubmit = async (e: FormEvent) => {
     e.preventDefault();
@@ -20,6 +22,7 @@ const Share = () => {
 
     const userId = await localStorage.getItem("userId");
     const userToken = await localStorage.getItem("userToken");
+    setLoading(true);
 
     try {
       const response = await api.post("/posts", {
@@ -31,46 +34,51 @@ const Share = () => {
       console.log("Resposta do servidor:", response.data);
     } catch (error) {
       console.error("Erro ao enviar o formulário:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <ShareContainer>
-      <div className="share">
-        <form className="container" onSubmit={(e) => handleOnSubmit(e)}>
-          <div className="top">
-            <img src={UserIcon} alt="" />
-            <div>
-              <input
-                type="text"
-                placeholder={`Escolha um título...`}
-                ref={titleRef}
-              />
-              <input
-                type="text"
-                placeholder={`Publicar Pedido de Projeto...`}
-                ref={contentRef}
-              />
+    <>
+      {loading && <Loader />}
+      <ShareContainer> 
+        <div className="share">
+          <form className="container" onSubmit={(e) => handleOnSubmit(e)}>
+            <div className="top">
+              <img src={UserIcon} alt="" />
+              <div>
+                <input
+                  type="text"
+                  placeholder={`Escolha um título...`}
+                  ref={titleRef}
+                />
+                <input
+                  type="text"
+                  placeholder={`Publicar Pedido de Projeto...`}
+                  ref={contentRef}
+                />
+              </div>
             </div>
-          </div>
-          <hr />
-          <div className="bottom">
-            <div className="left">
-              <input type="file" id="file" style={{ display: "none" }} />
-              <label htmlFor="file">
-                <div className="item">
-                  <img src={Image} alt="" />
-                  <span>Adicionar Imagem</span>
-                </div>
-              </label>
+            <hr />
+            <div className="bottom">
+              <div className="left">
+                <input type="file" id="file" style={{ display: "none" }} />
+                <label htmlFor="file">
+                  <div className="item">
+                    <img src={Image} alt="" />
+                    <span>Adicionar Imagem</span>
+                  </div>
+                </label>
+              </div>
+              <div className="right">
+                <button type="submit">Compartilhar</button>
+              </div>
             </div>
-            <div className="right">
-              <button type="submit">Compartilhar</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </ShareContainer>
+          </form>
+        </div>
+      </ShareContainer>
+    </>
   );
 };
 
