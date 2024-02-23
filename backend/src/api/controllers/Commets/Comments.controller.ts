1b0cc1fe6bd9152456a99159529fb1a30418ba04
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-02-23 20:53:14 +0000

    Adicionando comentarios sobre poo

--- backend/src/api/controllers/Commets/Comments.controller.ts
+++ backend/src/api/controllers/Commets/Comments.controller.ts
@@ -13,6 +13,7 @@ class Comments {
     const { postid } = req.params;
 
     try {
+      // Utilizando o método findAll para buscar comentários no banco de dados
       const comments = await Comment.findAll({
         where: { postid },
         include: [
@@ -40,6 +41,7 @@ class Comments {
     const { postid } = req.params;
   
     if (!postid) {
+      // Responde com erro se o parâmetro 'postid' não for fornecido na requisição
       res.status(400).json({ error: "O parâmetro 'postid' não foi fornecido na requisição" });
       return;
     }
@@ -47,6 +49,7 @@ class Comments {
     const { userid, content, token } = req.body;
   
     if (!userid || !content || !token) {
+      // Responde com erro se a requisição estiver incompleta
       res.status(400).json({ error: "Requisição Incompleta" });
       return;
     }
@@ -54,12 +57,13 @@ class Comments {
     const errors = validationResult(req);
   
     if (!errors.isEmpty()) {
+      // Loga os erros de validação e responde com erro 400
       logError("Erros de validação no comentário", res, 400);
       return;
     }
   
     try {
-      // Atualizado para associar o comentário ao post específico usando postid
+      // Cria um novo comentário associado ao post específico usando postid
       const comment = await Comment.create({
         postid,
         userid,
