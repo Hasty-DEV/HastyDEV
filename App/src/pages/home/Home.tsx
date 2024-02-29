c741e0838c87bedcd261dfe8c99eb593b8068c08
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-02-29 23:05:46 +0000

    Tirando o story

--- App/src/pages/home/Home.tsx
+++ App/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import Stories from "../../ui/components/stories/stories"
+
 import Posts from "../../ui/components/posts/Posts"
 import Share from "../../ui/components/share/Share"
 import "./home.scss"
@@ -6,7 +6,7 @@ import "./home.scss"
 const Home = () => {
   return (
     <div className="home">
-      <Stories/>
+      
       <Share/>
       <Posts/>
     </div>
