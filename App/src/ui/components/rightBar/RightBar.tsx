8cc8c438641e0790d1b3ce30d2641b05385af8cd
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-03-24 03:17:29 +0000

    iniciando a estilização do filter

--- App/src/ui/components/rightBar/RightBar.tsx
+++ App/src/ui/components/rightBar/RightBar.tsx
@@ -1,80 +1,101 @@
+import { useState } from "react";
 import RightBarContainer from "../../styles/rightBar/RightBar.styles";
 import UserLevelInfo from "../UserLevelInfo/UserLevelInfo";
+import styled from 'styled-components';
+import { SlArrowDown, SlArrowRight } from "react-icons/sl";
 
-const RightBar = () => {
+interface Filter {
+  name: string;
+  defaultVisible: boolean;
+  hasRemuneration: boolean;
+  categories: string[];
+}
+
+const filtersConfig: Filter[] = [
+  { name: "Linguagens", defaultVisible: false, hasRemuneration: false, categories: ["JavaScript", "Python", "Java"] },
+  { name: "Remuneração", defaultVisible: false, hasRemuneration: true, categories: ["Sim", "Não"] },
+  { name: "Categoria", defaultVisible: false, hasRemuneration: false, categories: ["Frontend", "Backend", "Full Stack"] },
+];
+
+const RightBar: React.FC = () => {
+  const [filterVisibility, setFilterVisibility] = useState<boolean[]>(filtersConfig.map(filter => filter.defaultVisible));
+
+  const toggleFilterVisibility = (index: number) => {
+    const newFilterVisibility = [...filterVisibility];
+    newFilterVisibility[index] = !newFilterVisibility[index];
+    setFilterVisibility(newFilterVisibility);
+  };
+
+
+
+
   return (
     <RightBarContainer>
-      <div className="rightBar">
-        <div className="container">
-          <UserLevelInfo />
-          <div className="item">
-            <span>Últimas Atividades</span>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Mudou a foto dele
-                </p>
-              </div>
-              <span>1 min atrás</span>
+  <div className="rightBar">
+    <div className="container">
+      <UserLevelInfo />
+      {filtersConfig.map((filter, filterIndex) => (
+        <TodoListContainer key={filterIndex}>
+          <div className="row align-items-center">
+            <div className="col">
+              <h1>
+                <button onClick={() => toggleFilterVisibility(filterIndex)}>{filter.name}</button>
+              </h1>
             </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Mudou a foto dele
-                </p>
-              </div>
-              <span>1 min atrás</span>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Mudou a foto dele
-                </p>
-              </div>
-              <span>1 min atrás</span>
-            </div>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <p>
-                  <span>Jane Doe</span> Mudou a foto dele
-                </p>
-              </div>
-              <span>1 min atrás</span>
+
+            
+              {filterVisibility[filterIndex] && (
+                <div>
+                  {filter.categories.map((category, categoryIndex) => (
+                    <label key={categoryIndex}>
+                      <input type="checkbox" />
+                      {category}
+                    </label>
+                  ))}
+                </div>
+              )}
+            
+
+            <div className="col-3 text-right align-self-center"> {/* Utilizing Bootstrap's grid system for positioning */}
+              <div>{filterVisibility[filterIndex] && <SlArrowDown />}
+                {!filterVisibility[filterIndex] && <SlArrowRight />}</div>
             </div>
           </div>
-          <div className="item">
-            <span>DEVS Online</span>
-            <div className="user">
-              <div className="userInfo">
-                <img
-                  src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="online" />
-                <span>Jane Doe</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </RightBarContainer>
+          <Line />
+        </TodoListContainer>
+      ))}
+    </div>
+  </div>
+</RightBarContainer>
+
+  
+
   );
 };
 
 export default RightBar;
+
+const TodoListContainer = styled.div`
+  margin-bottom: 10px;
+
+  h1 {
+    margin: 0;
+    font-size: 20px;
+  }
+
+  button {
+    background-color: #fff;
+    color: #000;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    outline: none;
+  }
+`;
+
+const Line = styled.hr`
+  width: 100%;
+  height: 1px;
+  border: none;
+  background-color: #000;
+`;
