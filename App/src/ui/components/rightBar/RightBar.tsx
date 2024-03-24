8a673e8a11e12d3478f884fff5a5a17e5e55dac6
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-03-24 03:14:39 +0000

    trocando

--- App/src/ui/components/rightBar/RightBar.tsx
+++ App/src/ui/components/rightBar/RightBar.tsx
@@ -1,34 +1,7 @@
-import { useState } from "react";
 import RightBarContainer from "../../styles/rightBar/RightBar.styles";
 import UserLevelInfo from "../UserLevelInfo/UserLevelInfo";
-import styled from 'styled-components';
-import { SlArrowDown, SlArrowRight } from "react-icons/sl";
 
-interface Filter {
-  name: string;
-  defaultVisible: boolean;
-  hasRemuneration: boolean;
-  categories: string[];
-}
-
-const filtersConfig: Filter[] = [
-  { name: "Linguagens", defaultVisible: false, hasRemuneration: false, categories: ["JavaScript", "Python", "Java"] },
-  { name: "Remuneração", defaultVisible: false, hasRemuneration: true, categories: ["Sim", "Não"] },
-  { name: "Categoria", defaultVisible: false, hasRemuneration: false, categories: ["Frontend", "Backend", "Full Stack"] },
-];
-
-const RightBar: React.FC = () => {
-  const [filterVisibility, setFilterVisibility] = useState<boolean[]>(filtersConfig.map(filter => filter.defaultVisible));
-
-  const toggleFilterVisibility = (index: number) => {
-    const newFilterVisibility = [...filterVisibility];
-    newFilterVisibility[index] = !newFilterVisibility[index];
-    setFilterVisibility(newFilterVisibility);
-  };
-
-
-
-
+const RightBar = () => {
   return (
     <RightBarContainer>
       <div className="rightBar">
@@ -105,28 +78,3 @@ const RightBar: React.FC = () => {
 };
 
 export default RightBar;
-
-const TodoListContainer = styled.div`
-  margin-bottom: 10px;
-
-  h1 {
-    margin: 0;
-    font-size: 20px;
-  }
-
-  button {
-    background-color: #fff;
-    color: #000;
-    border: none;
-    padding: 5px 10px;
-    cursor: pointer;
-    outline: none;
-  }
-`;
-
-const Line = styled.hr`
-  width: 100%;
-  height: 1px;
-  border: none;
-  background-color: #000;
-`;
