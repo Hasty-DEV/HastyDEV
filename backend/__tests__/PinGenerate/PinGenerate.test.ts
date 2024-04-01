e7b68cd2662618448c071b460c29a6283fafa0ef
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-04-01 23:28:37 +0000

    iniciando escrita de testes

---
+++ backend/__tests__/PinGenerate/PinGenerate.test.ts
@@ -0,0 +1,9 @@
+import { generatePinCode } from "../../src/utils/PinGenerate/PinGenerate";
+
+describe("Teste de Geração de PIN", () => {
+  it("Desde deve gerar um pin de 3 Caracteres em Maiúsculas", () => {
+    const pin = generatePinCode();
+    expect(pin.length).toBe(6);
+    expect(pin).toMatch(/^[A-Z0-9]+$/);
+  });
+});
