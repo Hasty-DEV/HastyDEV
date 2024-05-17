b736fc8ffa3e1fe1d537b21e1f7c6f057f88e7e9
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-05-17 23:30:02 +0000

    build: remove socket files

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,9 @@
 import server from "./server";
-import { SocketSetup } from "./config/Socket/Socket";
 import Jobs from "./loaders/jobs/jobs.loader";
 
 class App {
     public static Main() {
         server.StartServer();
-        SocketSetup();
         Jobs.Loader()
     }
 }
