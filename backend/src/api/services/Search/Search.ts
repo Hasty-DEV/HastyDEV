4ba94d8329f307fd04b99bf82c81d3c4b06d457d
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-05-19 04:25:59 +0000

    fix: change the format of sequelize

--- backend/src/api/services/Search/Search.ts
+++ backend/src/api/services/Search/Search.ts
@@ -1,31 +1,38 @@
 import { Op } from "sequelize";
 import Search from "../../models/Search/Search.model";
 
-class ItemService {
-  static async searchItems(searchTerm: string) {
-    const searchValue = `%${searchTerm}%`;
-
-    try {
-      const results = await Search.findAll({
-        where: {
-          [Op.or]: [
-            { title: { [Op.like]: searchValue } },
-            { subtitle: { [Op.like]: searchValue } },
-            { content: { [Op.like]: searchValue } },
-            { isPaid: true },
-            { photos: { [Op.like]: searchValue } },
-            { companyContent: { [Op.like]: searchValue } },
-            { categories: { [Op.like]: searchValue } },
-            { programmingLanguages: { [Op.like]: searchValue } },
-          ],
+const searchItems = async (searchTerm: string) => {
+  try {
+    const posts = await Search.findAll({
+      where: {
+        [Op.or]: {
+          title: {
+            [Op.like]: `%${searchTerm}%`,
+          },
+          subtitle: {
+            [Op.like]: `%${searchTerm}%`,
+          },
+          content: {
+            [Op.like]: `%${searchTerm}%`,
+          },
+          companyContent: {
+            [Op.like]: `%${searchTerm}%`,
+          },
+          categories: {
+            [Op.like]: `%${searchTerm}%`,
+          },
+          programmingLanguages: {
+            [Op.like]: `%${searchTerm}%`,
+          },
         },
-      });
-
-      return results;
-    } catch (err) {
-      throw new Error("Error executing search query:" + err);
-    }
+      },
+    });
+    
+    return posts;
+  } catch (error) {
+    console.error("Erro ao buscar posts:", error);
+    throw error;
   }
-}
+};
 
-export default ItemService;
+export default { searchItems };
