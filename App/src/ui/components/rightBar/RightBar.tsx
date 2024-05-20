bd48664d2204ef78d01778ab58c552d2b797e255
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-05-20 22:35:53 +0000

    style: Remove the filter of rightBar

--- App/src/ui/components/rightBar/RightBar.tsx
+++ App/src/ui/components/rightBar/RightBar.tsx
@@ -25,38 +25,6 @@ const RightBar = () => {
       <div className="rightBar">
         <div className="container">
           <UserLevelInfo />
-          {filtersConfig.map((filter, filterIndex) => (
-            <TodoListContainer key={filterIndex}>
-              <div className="row align-items-center">
-                <ContainerFilter className="col d-flex justify-content-between align-items-center " onClick={() => toggleFilterVisibility(filterIndex)}>
-                  <h1 className="m-0">
-                    <button className="w-100">
-                      {filter.name}
-                    </button>
-                  </h1>
-                  <div className="col-3 text-right">
-                    {filterVisibility[filterIndex] && <SlArrowDown />}
-                    {!filterVisibility[filterIndex] && <SlArrowRight />}
-                  </div>
-                </ContainerFilter>
-
-                {filterVisibility[filterIndex] && (
-                  <Categorias>
-                    {filter.categories.map((category, categoryIndex) => (
-                      <DivInput>
-                        <label key={categoryIndex}>
-                          <Input type="checkbox"  />
-                          {category}
-                        </label>
-
-                      </DivInput>
-                    ))}
-                  </Categorias>
-                )}
-              </div>
-              <Line />
-            </TodoListContainer>
-          ))}
         </div>
       </div>
     </RightBarContainer>
