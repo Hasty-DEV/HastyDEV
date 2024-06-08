de772553134f5a895e57b1883183f28fddc93425
Portela03 <136658564+Portela03@users.noreply.github.com>
2024-06-08 01:20:28 +0000

    feat: add the loading in the search

--- App/src/ui/components/posts/Posts.tsx
+++ App/src/ui/components/posts/Posts.tsx
@@ -2,14 +2,19 @@ import { useState, useEffect, useCallback } from "react";
 import PostsContainer from "../../styles/posts/Posts.styles";
 import Post from "../post/Post";
 
+
 import { PostType } from "../../../data/@types/Post/Post.type";
 import { api } from "../../../data/services/api";
+import Loader from "../Loader/Loader";
 
 type PostsProps = {
   searchTerm: string;
 };
 
 const Posts = ({ searchTerm }: PostsProps) => {
+  const [postFilter, setPostFilter] = useState<PostType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
   const getPostsFilter = async (searchTerm: string) => {
     try {
       const userToken = await localStorage.getItem("userToken");
@@ -19,7 +24,7 @@ const Posts = ({ searchTerm }: PostsProps) => {
         const response = await api.get(
           `/items/search?searchTerm=${searchTerm}`
         );
-        
+
         return response.data;
       }
     } catch (error) {
@@ -27,15 +32,17 @@ const Posts = ({ searchTerm }: PostsProps) => {
       throw error;
     }
   };
-  const [postFilter, setPostFilter] = useState<PostType[]>([]);
 
   const fetchPostsFilter = useCallback(async () => {
+    setLoading(true); // Define o estado de carregamento como verdadeiro antes de buscar os posts
     try {
       const response = await getPostsFilter(searchTerm);
       const reversedPosts = response.reverse();
       setPostFilter(reversedPosts);
     } catch (error) {
       console.error("Erro ao buscar os posts:", error);
+    } finally {
+      setLoading(false); // Define o estado de carregamento como falso após a busca dos posts, independentemente de sucesso ou erro
     }
   }, [searchTerm]);
 
@@ -46,9 +53,11 @@ const Posts = ({ searchTerm }: PostsProps) => {
   return (
     <PostsContainer className="d-flex flex-column">
       <h2 className="mb-3">Procure o Trabalho Ideal</h2>
-      {postFilter.map((post) => (
-        <Post post={post} key={post.postid} />
-      ))}
+      {loading ? (
+        <Loader /> // Renderiza o spinner de carregamento enquanto os posts estão sendo carregados
+      ) : (
+        postFilter.map((post) => <Post post={post} key={post.postid} />)
+      )}
     </PostsContainer>
   );
 };
