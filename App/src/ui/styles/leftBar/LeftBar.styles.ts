beb75ff24c448fde61583e3d8c93bb681412b159
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-06-11 23:10:26 +0000

    refactor: change style in leftbar

--- App/src/ui/styles/leftBar/LeftBar.styles.ts
+++ App/src/ui/styles/leftBar/LeftBar.styles.ts
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 const LeftBarContainer = styled.aside`
-  flex: 2.4;
-  position: sticky;
-  top: 80px;
-  height: calc(100vh - 70px);
+  flex: 2;
+  top: 65px;
+  height: 99vh;
+  padding-top: 30px;
   overflow: scroll;
   background-color: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.text};
-  box-shadow: 0px 10px 10px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 5px 0 10px rgba(0, 0, 0, 0.1);
+
 
   @media (max-width: 768px) {
     display: none;
