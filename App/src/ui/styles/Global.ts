07af6b5fce4f8ec5de5d8268cc770fd5930c493b
Jefferson Laurindo da Silva <107891837+Jefflds@users.noreply.github.com>
2024-06-13 23:00:05 +0000

    feat: style scrollbar

--- App/src/ui/styles/Global.ts
+++ App/src/ui/styles/Global.ts
@@ -13,6 +13,33 @@ const GlobalStyle = createGlobalStyle`
     color: ${(props) => props.theme.colors.text};
     overflow-x: hidden;
   }
+
+
+  ::-webkit-scrollbar {
+    width: 12px; 
+  }
+
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.background};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius: 20px;
+    border: 3px solid ${(props) => props.theme.colors.background}; 
+  }
+
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #4834d4;
+  }
+
+
+  ::-webkit-scrollbar-thumb:active {
+    background-color: #7f8fa6; 
+  }
+
 `;
 
 export default GlobalStyle;
