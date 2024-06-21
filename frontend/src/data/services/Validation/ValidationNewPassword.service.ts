d77c4c62748c43ba60f7b1a5dce9c16d5e777443
Lucas Quinteiro Campos <136658353+Lucasqc04@users.noreply.github.com>
2024-06-21 19:06:08 +0000

    arrumando bugs em resetpass

--- frontend/src/data/services/Validation/ValidationNewPassword.service.ts
+++ frontend/src/data/services/Validation/ValidationNewPassword.service.ts
@@ -1,14 +1,19 @@
-import * as Yup from "yup";
+import * as yup from "yup";
 
-export const validationNewPassword = Yup.object().shape({
-  email: Yup.string()
-    .email("Insira um e-mail válido")
-    .required("O e-mail é obrigatório"),
-  resetCode: Yup.string().required("O código de verificação é obrigatório"),
-  newPassword: Yup.string()
-    .required("A nova senha é obrigatória")
-    .min(6, "A senha deve conter pelo menos 6 caracteres"),
-  confirmNewPassword: Yup.string()
-    .required("Confirme a nova senha")
-    .oneOf([Yup.ref("newPassword"), ""], "As senhas devem ser iguais"),
+export const  validationNewPassword  = yup.object().shape({
+  password: yup
+    .string()
+    .required("A senha é obrigatória")
+    .min(6, "A senha deve ter pelo menos 6 caracteres")
+    .matches(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+    .matches(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+    .matches(/[0-9]/, "A senha deve conter pelo menos um número")
+    .matches(
+      /[@$!¨%*#?&]/,
+      "A senha deve conter pelo menos um caractere especial"
+    ),
+  confirmNewPassword: yup
+    .string()
+    .required("A confirmação da senha é obrigatória")
+    .oneOf([yup.ref("password")], "As senhas não coincidem"),
 });
